85cb7a403d9f29c9e64a29e580359039b444a451
chrismachado <christianomachado10@gmail.com>
2022-09-04 22:36:19 -0300

    rename: datasets vertebral column and artificial one working

--- util/datasets.py
+++ util/datasets.py
@@ -1,9 +1,9 @@
-import torch
-import matplotlib.pyplot as plt
+import numpy as np
+
 
 def vertebral_column(path=None):
     if not path:
-        path = 'data/column_2C.dat'
+        path = '../data/column_2C.dat'
     with open(path, "r") as f:
         _lines = [line.split(" ") for line in f.readlines()]
         data = []
@@ -17,36 +17,35 @@ def vertebral_column(path=None):
                 else:
                     data.append(float(line))
 
-        data = torch.reshape(torch.tensor(data), (310, 6))
-        return data, torch.tensor(target, dtype=int)
+        data = np.reshape(data, (310, 6))
+        return data, np.array(target)
 
 
-def artificial_i(n, noise=0.2):
-    _noises_p1 = torch.distributions.uniform.Uniform(-noise, noise).sample([n, 2])
-    _noises_p2 = torch.distributions.uniform.Uniform(-noise, noise).sample([n, 2])
-    _noises_p3 = torch.distributions.uniform.Uniform(-noise, noise).sample([n, 2])
-    _noises_p4 = torch.distributions.uniform.Uniform(-noise, noise).sample([n, 2])
+def artificial_one(n, noise=0.2):
+    _noises_p1 = np.random.uniform(-noise, noise, (n, 2))
+    _noises_p2 = np.random.uniform(-noise, noise, (n, 2))
+    _noises_p3 = np.random.uniform(-noise, noise, (n, 2))
+    _noises_p4 = np.random.uniform(-noise, noise, (n, 2))
 
-    _ones = torch.ones((n, 1))
-    _zeros = torch.zeros((n, 1))
+    _ones = np.ones((n, 1))
+    _zeros = np.zeros((n, 1))
 
-    _cls_100 = torch.zeros((n, 2))
-    _cls_101 = torch.stack((_zeros, _ones), 2)
-    _cls_111 = torch.ones((n, 2))
-    print(_cls_101.shape)
+    _cls_100 = np.zeros((n, 2)) + _noises_p1
+    _cls_101 = np.reshape(np.stack((_zeros, _ones), 2), (n, 2)) + _noises_p2
+    _cls_110 = np.reshape(np.stack((_ones, _zeros), 2), (n, 2)) + _noises_p2
+    _cls_111 = np.ones((n, 2)) + _noises_p4
 
-    # samples = torch.rand((n, 2))
-    # return torch.cat((_cls_100))
-    return torch.cat((_cls_100, _cls_111))
+    _cls_0s = _cls_100.shape[0] + _cls_101.shape[0] + _cls_110.shape[0]
+    _cls_1s = _cls_111.shape[0]
 
+    samples = np.concatenate((_cls_100, _cls_101, _cls_110, _cls_111))
+    targets = np.concatenate((np.zeros((_cls_0s,), dtype=int), np.ones((_cls_1s,), dtype=int)))
 
-if __name__ == '__main__':
-    s = artificial_i(25)
-    # print(s)
-
-    plt.scatter(s[:, 0], s[:, 1])
-    plt.show()
+    return samples, targets
 
 
+def train_test_split(samples, targets, size):
+    tt_size = int(targets.shape[0] * size)
+    st_size = int(samples.shape[0] * size)
 
-
+    return samples[:st_size, :], samples[st_size:, :], targets[:tt_size], targets[tt_size:]
